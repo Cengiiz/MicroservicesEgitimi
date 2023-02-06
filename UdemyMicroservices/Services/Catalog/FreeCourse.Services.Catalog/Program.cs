24f30b65537e9c403b8bdcb1d2a29ab853f7384f
Cengiiz <dadem64@gmail.com>
2023-02-06 21:17:41 +0300

    appsetting.json/program.cs

--- UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,3 +1,6 @@
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.Configure<DataBaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddSingleton<IDataBaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DataBaseSettings>>().Value;
+});
 
 
 
