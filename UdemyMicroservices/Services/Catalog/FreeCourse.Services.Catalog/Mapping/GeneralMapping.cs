ffb1958282e2e56d6e402a5abe02eca2a385a482
Cengiiz <dadem64@gmail.com>
2023-02-06 21:00:11 +0300

    AutoMapper Library

---
+++ UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Model;
+
+namespace FreeCourse.Services.Catalog.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course,CourseDto>().ReverseMap();
+            CreateMap<Course, CourceCreatDto>().ReverseMap();
+            CreateMap<Course, CourceUpdateDto>().ReverseMap();
+
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+
+
+
+        }
+    }
+}
