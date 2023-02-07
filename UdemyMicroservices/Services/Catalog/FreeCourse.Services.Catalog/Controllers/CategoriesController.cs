dde95d23b5f73a50196bca77df69f5e5fbd8d969
Cengiiz <dadem64@gmail.com>
2023-02-07 16:33:16 +0300

    CategoriesController

---
+++ UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using FreeCource.Shared.ControllerBases;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+            
+        }
+        
+
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _categoryService.GetAllAsync();
+            return CreateActionResultInsance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _categoryService.GetByIdAsync(id);
+            return CreateActionResultInsance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryCreateDto categoryCreateDto)
+        {
+            var response = await _categoryService.CreateAsync(categoryCreateDto);
+            return CreateActionResultInsance(response);
+        }
+
+
+
+
+
+    }
+}
